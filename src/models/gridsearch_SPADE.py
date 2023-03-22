1606b863cf68cfb96473aab39e841c75d7be70f8
ValColl <val.collumeau@gmail.com>
2023-03-22 22:07:33 +0100

    Saved results

--- src/models/gridsearch_SPADE.py
+++ src/models/gridsearch_SPADE.py
@@ -14,27 +14,42 @@ for top_k in [2, 5, 10]:
     # Launch main with top_k and class_names
     main(top_k=top_k, class_names=class_names)
 
-# Load the results
-results = pd.read_csv('results.csv')
-
-# Plot the image and pixel accuracies for each class_name and each top_k
+## Load the results
+# Create a double dictionary to store the results with keys class_name and top_k
+image_rocauc = {}
+pixel_rocauc = {}
 for class_name in class_names:
-    plt.plot(results[results['class_name'] == class_name]['top_k'], results[results['class_name'] == class_name]['image_accuracy'], label=class_name)
-plt.legend()
-plt.xlabel('top_k')
-plt.ylabel('image_accuracy')
-plt.show()
-# Save the plot
-plt.savefig('grid_image_accuracies.png')
+    image_rocauc[class_name] = {}
+    pixel_rocauc[class_name] = {}
+avg_image_rocauc = {}
+avg_pixel_rocauc = {}
+# Read each line of the file and store it in a list
+with open('../../models/SPADE/results.csv', 'r') as f:
+    lines = f.readlines()
+# Iterate over the lines
+for line in lines[1:]:
+    # Split the line by spaces
+    line = line.strip().split(',')
+    print(line)
+    # Get the class name, top_k, and the AUC value
+    top_k = int(line[0])
+    for i, class_name in enumerate(class_names):
+        image_rocauc[class_name][top_k] = float(line[2*i+1])
+        pixel_rocauc[class_name][top_k] = float(line[2*i+2])
+    avg_image_rocauc[top_k] = float(line[-2])
+    avg_pixel_rocauc[top_k] = float(line[-1])
 
+
+print(image_rocauc)
+
+# Plot the image_rocauc and average_rocauc for each class
 for class_name in class_names:
-    plt.plot(results[results['class_name'] == class_name]['top_k'], results[results['class_name'] == class_name]['pixel_accuracy'], label=class_name)
-plt.legend()
+    plt.plot(list(image_rocauc[class_name].keys()), list(image_rocauc[class_name].values()), label=class_name)
+plt.plot(list(avg_image_rocauc.keys()), list(avg_image_rocauc.values()), label='Average')
 plt.xlabel('top_k')
-plt.ylabel('pixel_accuracy')
-plt.show()
+plt.ylabel('Image ROC AUC')
+plt.title('Image ROC AUC for each class')
+plt.legend()
+
 # Save the plot
-plt.savefig('grid_pixel_accuracy.png')
-
-
-
+plt.savefig('../../models/SPADE/image_rocauc.png')
